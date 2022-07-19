531fec934760a6d2314ff27018669e69d8c1ae63
melodytowett <melodytowett99@gmail.com>
2022-07-19 10:33:41 +0300

    react paren and child components

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,15 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child({ onChangeColor }) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ onChangeColor,color }) {
+  console.log(onChangeColor);
+  function handleClick() {
+    const newCOlor = getRandomColor();
+    onChangeColor(newCOlor);
+  }
+  return (
+    <div onClick={handleClick} className="child" style={{ backgroundColor: color }} />
+  );
 }
 
 export default Child;
